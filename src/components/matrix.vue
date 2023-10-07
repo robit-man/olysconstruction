b2d416602c9e62de947d803e7b8455f87cc71a26
robit <36677806+robit-man@users.noreply.github.com>
2023-10-07 08:19:34 +0000

    OPOE

--- src/components/matrix.vue
+++ src/components/matrix.vue
@@ -18,45 +18,40 @@
     canvas.value.height = ch;
   
     const charArr = "abcdefghijklmnopqrstuvwxyz123456789АВГДЄЅЗИѲІКЛМНПЧРСТѴФХЦ".split('');
-    let maxCharCount = 300;
+    let maxCharCount = 200;
     let fallingCharArr = [];
-    let fontSize = 20;
+    let maxFontSize = 20;
+    let fontSize = maxFontSize;
     let maxColumns = cw / fontSize;
     let frames = 0;
     let mouseX = 0;
     let mouseY = 0;
   
     class FallingChar {
-      constructor(x, y) {
+      constructor(x, y, z) {
         this.x = x;
         this.y = y;
+        this.z = z;
+        this.fontSize = maxFontSize - this.z * 10; 
       }
   
       draw(ctx) {
         this.value = charArr[Math.floor(Math.random() * charArr.length)].toUpperCase();
-        this.speed = (Math.random() * fontSize * 3) / 4 + (fontSize * 3) / 4;
-  
-        const distance = Math.hypot(this.x - mouseX, this.y - mouseY);
+        this.speed = (Math.random() * this.fontSize * 3) / 4 + (this.fontSize * 3) / 4;
   
         ctx.fillStyle = "rgba(255,200,0)";
-        ctx.font = fontSize + "px 'Handjet', sans-serif";
+        ctx.font = this.fontSize + "px 'Handjet', sans-serif";
   
-        if (distance < 50) {
-          ctx.shadowBlur = 25;
-          ctx.shadowColor = "rgba(255,200,0,1)";
-        } else {
-          ctx.shadowBlur = 15;
-          ctx.shadowColor = "rgba(255,200,0,0.8)";
-        }
+        let tiltX = this.x + (mouseX - cw / 2) * this.z / 50; 
+        let tiltY = this.y + (mouseY - ch / 2) * this.z / 50; 
   
-        ctx.fillText(this.value, this.x, this.y);
-        ctx.shadowBlur = 0;
+        ctx.fillText(this.value, tiltX, tiltY);
   
         this.y += this.speed;
   
         if (this.y > ch) {
-          this.y = (Math.random() * ch) / 2 - 50;
-          this.x = Math.floor(Math.random() * maxColumns) * fontSize;
+          this.y = 0; 
+          this.x = Math.floor(Math.random() * maxColumns) * this.fontSize;
         }
       }
     }
@@ -65,7 +60,8 @@
       if (fallingCharArr.length < maxCharCount) {
         fallingCharArr.push(new FallingChar(
           Math.floor(Math.random() * maxColumns) * fontSize,
-          (Math.random() * ch) / 2 - 50
+          0,
+          Math.random() * 5 
         ));
       }
   
@@ -85,7 +81,7 @@
       ch = window.innerHeight;
       canvas.value.width = cw;
       canvas.value.height = ch;
-      maxColumns = cw / fontSize;
+      maxColumns = cw / maxFontSize;
     };
   
     const mouseMoveListener = (event) => {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,267 @@
 <template>
     <CanvasAnimation />
+    <div class="front" style="width:512px;height:512px;position:absolute;left:calc(50vw - 256px);top:5vh;">
+      <h1 v-if="finished" style="color:rgba(255,200,0);">{{ finalText }}</h1>
+      <h1 v-if="!finished" :style="{color: 'rgba(255,200,0)', position: 'relative', margin: 0}" v-html="html"></h1>
+<div class="ring">
+    <div class="swap"><img src="swap.svg" style="animation:spin 5s ease infinite;width:64px;height:64px;position:absolute;right:3rem;top:-3rem;" alt=""></div>
+    <div class="doge"><img style="width:256px;position:absolute;left:48px;top:20px;opacity:0.5;" src="doge.png" alt=""></div>
+      <div class="eth"><div class="pyramid">
+    <div class="square">
+      <div class="triangle"></div>
+      <div class="triangle"></div>
+      <div class="triangle"></div>
+      <div class="triangle"></div>
+    </div>
+  </div>
+  
+  <div class="pyramid inverse">
+    <div class="square">
+      <div class="triangle"></div>
+      <div class="triangle"></div>
+      <div class="triangle"></div>
+      <div class="triangle"></div>
+    </div></div>
+  </div></div>
+
+  <div class="copy"><p>Stealth launched as a memecoin and with no promises or expectations, we decided to build a new protocol which solves the multichain bridge problem for non-EVM compatible systems. Our experienced team has created invaluable depth to human capital both tangible and intangible. Try our bridge today!</p></div>
+
+  <div class="row" style="display:flex;flex-flow:wrap;gap:1rem;justify-content: space-between;margin-top:2rem;">
+
+    <a href="https://etherscan.io/token/0x33caf58d14d7cd284cc2d7f2bc878d2d63c8956a" targat="_blank"><img style="height:48px;margin:8px;filter:hue-rotate(210deg)brightness(2);" src="ether-scan.png" alt=""><p>ETHERSCAN</p></a>
+    <a href="https://www.dextools.io/app/en/ether/pair-explorer/0xf60028379c813a559711bd7bd0b238261b9a648f" targat="_blank"><img style="height:64px;width:64px;filter:hue-rotate(185deg)brightness(4);" src="dextools_logo.png" alt=""><p>DEXTOOLS</p></a>
+    <a href="https://t.me/OPOEthereum" targat="_blank"><img style="height:48px;width:48px;margin:8px;filter:invert(1)hue-rotate(185deg)brightness(4);" src="telegram.svg" alt=""><p>TELEGRAM</p></a>
+    <a href="https://twitter.com/OPOEthereum" targat="_blank"><img style="height:48px;width:48px;margin:8px;filter:invert(1)hue-rotate(185deg)brightness(4);" src="x.png" alt=""><p>X CORP</p></a>
+  </div>
+    </div>
   </template>
   
   <script>
+  import { ref, onMounted, onUnmounted } from 'vue';
   import CanvasAnimation from '@/components/matrix.vue'; // Update with your actual path
   
   export default {
     components: {
-      CanvasAnimation
-    }
+      CanvasAnimation,
+    },
+    setup() {
+      const finalText = 'ONLY POSSIBLE ON ETHEREUM';
+      const html = ref('');
+      const finished = ref(false);
+  
+      const randomChar = () => {
+        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        return chars[Math.floor(Math.random() * chars.length)];
+      };
+  
+      const type = (index) => {
+        if (index === finalText.length) {
+          finished.value = true;
+          return;
+        }
+  
+        let text = finalText.substr(0, index);
+        text = text.replace(/./g, '<span style="color: rgba(255,200,0);">$&</span>');
+        for (let i = 0; i < 5; i++) {
+          setTimeout(() => {
+            html.value = text + '<span style="color: rgba(255,200,0);">' + randomChar() + '</span>' + finalText.substr(index + 1).replace(/./g, '<span style="color: transparent;">$&</span>');
+          }, i * 80);
+        }
+  
+        setTimeout(() => {
+          html.value = finalText.substr(0, index + 1).replace(/./g, '<span style="color: rgba(255,200,0);">$&</span>') +
+            finalText.substr(index + 1).replace(/./g, '<span style="color: transparent;">$&</span>');
+          type(index + 1);
+        }, 500);
+      };
+      onMounted(() => {
+  type(0);
+  
+  const ethElement = document.querySelector('.eth');
+  
+  const updateRotation = (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+    
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+    
+    const deltaX = x - centerX;
+    const deltaY = y - centerY;
+    
+    const rotationY = deltaX / centerX * 20; // Adjust the multiplier value to control the rotation intensity
+    const rotationX = -(deltaY / centerY * 20); // Adjust the multiplier value to control the rotation intensity
+    
+    ethElement.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+  };
+  
+  window.addEventListener('mousemove', updateRotation);
+  
+  onUnmounted(() => {
+    window.removeEventListener('mousemove', updateRotation);
+  });
+});
+  
+      return { html, finalText, finished };
+    },
   };
   </script>
   
-  <style>
-   @import url('https://fonts.googleapis.com/css2?family=Handjet:wght@300&display=swap');
-   </style>
+  <style scoped lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Handjet:wght@300&display=swap');
+  
+  * {
+    font-family: 'Handjet', sans-serif;
+  }
+  p{    font-family: 'Handjet', sans-serif;
+color:rgba(255,200,0,1);}
+  a{margin:auto;display:flex;flex-flow:row;p{margin:auto;font-size:32px;}}
+$size: 5rem;
+$rsize: $size * .75;
+$angle: 68deg;
+$r: 50deg;
+$color: #78540092;
+$bg: darken($color, 5);
+
+body {
+  height: 100vh;
+  display: grid;
+  background: $bg;
+  > * {
+    margin: auto;
+  }
+}
+
+h1 {text-align:center;
+  font-weight: 100;
+  color: white;
+}
+
+.ring{overflow:hidden;margin:auto;margin-bottom:2rem;margin-top:2rem;display:flex;flex-flow:column;justify-content:center;width:256px;height:256px;backdrop-filter:blur(5px)brightness(1.5);box-shadow:inset 0px 0px 10px  rgba(255,200,0,1), 0px 0px 10px  rgba(255,200,0,1); border:5px solid rgba(255,200,0,1);border-radius:1000px;}
+
+
+
+@keyframes rotate {
+  from {
+    transform: rotateX($r) rotateZ(45deg) translateZ(-.5rem);
+  }
+  50% {
+    transform: rotateX($r) rotateZ(225deg) translateZ(.5rem);
+  }
+  100% {
+    transform: rotateX($r) rotateZ(405deg) translateZ(-.5rem);
+  }
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  10% {    
+    transform: rotate(-20deg);
+
+  }
+  90% {    
+    transform: rotate(380deg);
+
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.copy{text-align:center;}
+
+.eth {position:relative;
+    margin-top:1rem;margin-left:2rem;
+  transform-style: preserve-3d;
+  width: $rsize; height: $rsize;
+  transform-origin: $rsize / 2 $rsize / 2;
+  transform: rotateX($r) rotateZ(45deg);
+  animation: rotate 4s linear infinite;
+  &:hover {
+   // animation-play-state: paused;
+  }
+}
+
+.pyramid {
+  position: absolute;
+  perspective: 500px;
+  transform-style: preserve-3d;
+  &.inverse {
+    transform: translateZ(-$size / 7) rotateY(180deg);
+  }
+}
+
+.square {
+  width: $rsize;
+  height: $rsize;
+  background: lighten($color, 20);;
+  transform-style: preserve-3d;
+}
+
+.triangle {
+  position: absolute;
+  width: $size;
+  height: $size;
+  transition:all 0.2s ease;
+  &:hover{opacity:0.2;}
+  &:nth-child(1) {
+    width: $rsize;
+    top: -33%;
+    background: lighten($color, 30);
+    clip-path: polygon(50% 0%, 0% 100%, 100% 100%);
+    transform-origin: 50% 100%;
+    transform: rotateX(-$angle);
+  }
+  &:nth-child(2) {
+    width: $rsize;
+    background: lighten($color, 30);
+    clip-path: polygon(50% 100%, 0 0, 100% 0);
+    transform-origin: 50% 0%;
+    transform: rotateX($angle);
+  }
+  &:nth-child(3) {
+    height: $rsize;
+    left: -33%;
+    background: lighten($color, 40);
+    transform-origin: 100% 50%;
+    clip-path: polygon(100% 100%, 0 50%, 100% 0);
+    transform: rotateY($angle);
+  }
+  &:nth-child(4) {
+    height: $rsize;
+    background: lighten($color, 40);
+    transform-origin: 0% 50%;
+    clip-path: polygon(0 100%, 100% 50%, 0 0);
+    transform: rotateY(-$angle);
+  }
+}
+
+// Other pens links
+
+.other-pens {
+  position: absolute;
+  bottom: 1rem;
+  strong {
+    font-family: Helvetica, Arial, sans-serif;
+    font-weight: bold;
+    margin: auto;
+    display: block;
+    text-align: center;
+  }
+  li, a, strong {
+    color: rgba(255,255,255,.8);
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+    margin-left: .8rem;
+  }
+  a {
+    display: block;
+    padding: .4rem;
+  }
+}
+  </style>
+  
